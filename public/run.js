ccb23902f4cf7f0db43673658c66a3d93b0b05e8
saoodmohd <mshakeel4@slb.com>
2018-06-01 11:33:19 +0530

    before pull

--- public/run.js
+++ public/run.js
@@ -33,6 +33,8 @@ function getTask(taskName) {
 // }
 
 var req = []
+datapoints = []
+
 function addTask(){
     newTask = getTask("<tr><td>Task</td></tr>");
     req.push(newTask);
@@ -62,7 +64,17 @@ function addProcess(){
     entryProcess.setAttribute("style","");
     document.getElementById("processList").appendChild(entryProcess);
     processCapacity.setAttribute("onchange", "addCapacityToProcess(this.id, this.value)");
-   // document.getElementById("numberOfProcess").innerHTML = pro.length;   
+   // document.getElementById("numberOfProcess").innerHTML = pro.length;
+
+    var dataPoint = {
+        name: "Process",
+        type: "spline",
+        showInLegend: true,
+        dataPoints: [
+            // { x: 0, y: 31 }
+        ]
+    }
+    data.push(dataPoint)
 } 
 
 
@@ -111,12 +123,114 @@ function oneTimeStep(){
             cycleTimeDict[task["id"]][0] = currentTime;
         }
     }
+
+    setData(getPoints());
+    buildChart();
+}
+
+function setData(points){
+    for(var i=0 ; i<points.length;i++){
+        data[i]["dataPoints"].push({
+            x:currentTime,
+            y:points[i]
+        })
+    }
+}
+
+function getPoints(){
+    var points = []
+    var tot = 0;
+    for(var i=pro.length-1;i>=0;i++){
+        tot = tot + pro[i].completedQueue.length
+        points.push(tot);
+    }
+    return points;
 }
 
 // pro = getProcessproay(3);
 req = []
-
+data = []
 // for(var i =0;i<20;i++){
 //     req.push(getTask(i+""))
 // }
+function buildChart(){
+    var chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        title:{
+            text: "Daily High Temperature at Different Beaches"
+        },
+        axisX: {
+            valueFormatString: "DD MMM,YY"
+        },
+        axisY: {
+            title: "Temperature (in °C)",
+            includeZero: false,
+            suffix: " °C"
+        },
+        legend:{
+            cursor: "pointer",
+            fontSize: 16,
+            itemclick: toggleDataSeries
+        },
+        toolTip:{
+            shared: true
+        },
+        data:[]
+        // data: [{
+        //     name: "Myrtle Beach",
+        //     type: "spline",
+        //     showInLegend: true,
+        //     dataPoints: [
+        //         { x: new Date(2017,6,24), y: 31 },
+        //         { x: new Date(2017,6,25), y: 31 },
+        //         { x: new Date(2017,6,26), y: 29 },
+        //         { x: new Date(2017,6,27), y: 29 },
+        //         { x: new Date(2017,6,28), y: 31 },
+        //         { x: new Date(2017,6,29), y: 30 },
+        //         { x: new Date(2017,6,30), y: 29 }
+        //     ]
+        // },
+        // {
+        //     name: "Martha Vineyard",
+        //     type: "spline",
+        //     yValueFormatString: "#0.## °C",
+        //     showInLegend: true,
+        //     dataPoints: [
+        //         { x: new Date(2017,6,24), y: 20 },
+        //         { x: new Date(2017,6,25), y: 20 },
+        //         { x: new Date(2017,6,26), y: 25 },
+        //         { x: new Date(2017,6,27), y: 25 },
+        //         { x: new Date(2017,6,28), y: 25 },
+        //         { x: new Date(2017,6,29), y: 25 },
+        //         { x: new Date(2017,6,30), y: 25 }
+        //     ]
+        // },
+        // {
+        //     name: "Nantucket",
+        //     type: "spline",
+        //     yValueFormatString: "#0.## °C",
+        //     showInLegend: true,
+        //     dataPoints: [
+        //         { x: new Date(2017,6,24), y: 22 },
+        //         { x: new Date(2017,6,25), y: 19 },
+        //         { x: new Date(2017,6,26), y: 23 },
+        //         { x: new Date(2017,6,27), y: 24 },
+        //         { x: new Date(2017,6,28), y: 24 },
+        //         { x: new Date(2017,6,29), y: 23 },
+        //         { x: new Date(2017,6,30), y: 23 }
+        //     ]
+        // }]
+    });
+    chart.render();
+}
 
+
+function toggleDataSeries(e){
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else{
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
